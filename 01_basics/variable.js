90666f41202ad24bd95cb855567228662355d941
Ahmad-Raza810 <mustafaraza03898@gmail.com>
2024-10-30 07:04:02 +0000

    about variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,20 @@
+const studentId="432"
+let studentName="ahmad raza"
+var studentClass="higher education"
+studentCity="delhi"
+let studentScore;
+var grade;
+
+console.log("name is:",studentName)
+console.table([studentName,studentClass,studentCity,studentId,studentScore,grade])
+
+
+
+
+
+
+
+//studentId=123//this is not allowable
+
+//do not use var due to scope related problem.be a better problem
+

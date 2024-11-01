dd6f3cf0c405065d64d13051640fc5a0ea3d1133
Ahmad Raza <mustafaraza03898@gmail.com>
2024-11-01 05:49:29 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,11 @@
+let myDate=new Date()
+/* console.log(myDate)
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+ */
+
+let mydate2=new Date(2024,-1,2)
+console.log(mydate2.toLocaleString());
+
+

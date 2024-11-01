804c8189878271d044c2bec87fcbad6fc36b4c03
Ahmad Raza <mustafaraza03898@gmail.com>
2024-11-01 05:18:42 +0000

    number

---
+++ 01_basics/number.js
@@ -0,0 +1,11 @@
+let score=300
+let score2=new Number("800")
+console.log(typeof score2 );
+console.log(score2.toString()[0]);
+
+console.log(typeof score.toString())
+console.log(typeof score);
+let point=123.456
+let s=1000000
+console.log(point.toPrecision(1));
+console.log(s.toLocaleString("en-In"))

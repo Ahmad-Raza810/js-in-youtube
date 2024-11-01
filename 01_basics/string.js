c8feb59b5a2c3762ac5e1eee7606ce5b0789b70c
Ahmad Raza <mustafaraza03898@gmail.com>
2024-11-01 04:00:41 +0000

    about string

---
+++ 01_basics/string.js
@@ -0,0 +1,27 @@
+let name="ahmad"
+let st=`good morning ${name}`
+
+let yourname=new String("myname")
+console.log(yourname[6909])
+
+console.log(name.length)
+console.log(name.charAt(4));
+
+ //if there is no specified element then retur -1
+//give index of first occurence
+console.log(name.indexOf('x'));
+
+console.log(name.substring(0,1))
+
+console.log(st.split('o'));
+
+
+let url="         this is a url    "
+console.log(url.trim())
+
+//if there are duplicate elements replace first occurence of element.
+//if element does not exist then it return string as it is.
+console.log(name.replace('x','o'))
+
+
+console.log(name.includes("aa"));

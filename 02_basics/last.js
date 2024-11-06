3c0420324375d9f800b8b2dd115edea98939cb29
Ahmad Raza <mustafaraza03898@gmail.com>
2024-11-06 06:02:09 +0000

    end of part one

---
+++ 02_basics/last.js
@@ -0,0 +1,24 @@
+//creating map
+//map contains unique values
+//iterator
+//prevent insertion order
+const myMap=new Map()
+myMap.set("name","kuch bhi")
+myMap.set("kaam","code krna")
+myMap.set(1,2)
+//console.log(myMap);
+
+for (const [key,name] of myMap) {
+   // console.log(key,name);
+    
+}
+
+
+let test=[100,200,300,400]
+test.forEach((item)=>{console.log(item)})
+
+
+
+
+
+
